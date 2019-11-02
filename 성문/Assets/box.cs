276fe7e7e51d54e7e828f07d9ddcb374234a6078
ratroy3 <57277386+ratroy3@users.noreply.github.com>
2019-11-02 11:01:36 +0900

    킹덤

---
+++ 성문/Assets/box.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class box : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 5;
+    float h;
+    float v;
+    Rigidbody2D rigid;
+    bool isHorizonMove;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonDown("Horizontal");
+        bool vUp = Input.GetButtonDown("Vertical");
+
+        if(hDown || vUp)
+        {
+            isHorizonMove = true;
+        }
+        else if (vDown || hUp)
+        {
+            isHorizonMove = false;
+        }
+    }
+    void FixedUpdate()
+    {
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = new Vector2(h, v) * speed;
+    }
+}
